ec034ea82cf9fc59b3daad246f705c0c4e2f5b3a
Kaan <kaangokdemir01@gmail.com>
2020-08-14 18:05:20 +0300

    bugfix

--- version.js
+++ version.js
@@ -13,65 +13,60 @@ const jsonfile = require('jsonfile')
  * @param {number=4} [spaces] number of spaces to format jsonfile (set to 0 to remove all spaces)
  * @return {object.<string, string>} returns { original, updated }
  */
-module.exports = function version(filename, options)
-{
-    options = options || {}
-    if (!options.replace && !options.major && !options.minor && !options.patch)
-    {
-        options.patch = 1
+module.exports = function version(filename, options) {
+  options = options || {}
+  if (!options.replace && !options.major && !options.minor && !options.patch) {
+    options.patch = 1
+  }
+  options.spaces = typeof options.spaces === 'undefined' ? 4 : options.spaces
+  let json
+  try {
+    if (filename.indexOf('/') === -1 && filename.indexOf('\\') === -1) {
+      filename = process.cwd() + '/' + filename
     }
-    options.spaces = typeof options.spaces === 'undefined' ? 4 : options.spaces
-    let json
-    try
-    {
-        if (filename.indexOf('/') === -1 && filename.indexOf('\\') === -1)
-        {
-            filename = process.cwd() + '/' + filename
-        }
-        json = jsonfile.readFileSync(filename)
+    json = jsonfile.readFileSync(filename)
+  } catch (e) {
+    console.error('ERROR opening file ' + filename + ' (' + e.error + ')')
+    process.exit(1)
+  }
+  options.entry = options.entry || 'version'
+  const current = json[options.entry]
+  let updated, split
+  if (options.replace) {
+    json[options.entry] = options.entry
+    updated = options.entry
+  } else {
+    split = current.split('.')
+    if (split.length !== 3) {
+      console.warn(
+        'WARNING version in ' +
+          filename +
+          ' was not in MAJOR.MINOR.PATCH format'
+      )
+      for (let i = split.length; i < 3; i++) {
+        split[i] = '0'
+      }
     }
-    catch (e)
-    {
-        console.error('ERROR opening file ' + filename + ' (' + e.error + ')')
-        process.exit(1)
+    if (options.major) {
+      split[0] = parseInt(split[0]) + options.major
+      split[1] = '0'
+      split[2] = '0'
+    } else if (options.minor) {
+      split[1] = parseInt(split[1]) + options.minor
+      split[2] = '0'
+    } else if (options.patch) {
+      split[2] = parseInt(split[2]) + options.patch
     }
-    options.entry = options.entry || 'version'
-    const current = json[options.entry]
-    let updated, split
-    if (options.replace)
-    {
-        json[options.entry] = options.entry
-        updated = options.entry
-    }
-    else
-    {
-        split = current.split('.')
-        if (split.length !== 3)
-        {
-            console.warn('WARNING version in ' + filename + ' was not in MAJOR.MINOR.PATCH format')
-            for (let i = split.length; i < 3; i++)
-            {
-                split[i] = '0'
-            }
-        }
-        if (options.major)
-        {
-            split[0] = parseInt(split[0]) + options.major
-            split[1] = '0'
-            split[2] = '0'
-        }
-        else if (options.minor)
-        {
-            split[1] = parseInt(split[1]) + options.minor
-            split[2] = '0'
-        }
-        else if (options.patch)
-        {
-            split[2] = parseInt(split[2]) + options.patch
-        }
-    }
-    json[options.entry] = parseInt(split[0]) + '.' + parseInt(split[1]) + '.' + parseInt(split[2])
-    updated = json[options.entry]
-    jsonfile.writeFileSync(filename, json, { spaces: options.spaces})
-    return { original: current, updated }
+  }
+
+  if (split) {
+    json[options.entry] =
+      parseInt(split[0]) + '.' + parseInt(split[1]) + '.' + parseInt(split[2])
+  } else {
+    json[options.entry] = options.replace
+  }
+
+  updated = json[options.entry]
+  jsonfile.writeFileSync(filename, json, { spaces: options.spaces })
+  return { original: current, updated }
 }
